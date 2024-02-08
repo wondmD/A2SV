e476ac67ae3e1d5364850ea2c78e2215864cfbf9
Wondmeneh Dereje <99288646+wondmD@users.noreply.github.com>
2024-02-08 15:09:05 +0300

    Added solution for Subarray Sum Equals K

--- camp/week1/subarray-sum-equals-k.py
+++ camp/week1/subarray-sum-equals-k.py
@@ -1,16 +1,15 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        count=0
-        dictn={}
-        for i in accumulate(nums):
-            if i-k in dictn:
-                count+=dictn[i-k]
-            if i==k:
-                count+=1
-            if i in dictn:
-                dictn[i]+=1
-            else:
-                dictn[i]=1
-        return count
-        
+        tot = 0
+        res = 0
+        dic = {0:1}
+        for num in nums:
+            tot += num
+            diff  = tot - k
+            if diff in dic:
+                res += dic[diff]
+            dic[tot] = 1 + dic.get(tot, 0)
+        return (res)
+
+
             
